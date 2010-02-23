8b3804e2992d9f3d340503c3aaa4c67f10a8f101
Mark Meves <mark.meves@gmail.com>
2010-02-23 06:46:23 -0500

    * specs for basic find wrapper

--- Support/external/ack-lite/ack-lite.rb
+++ Support/external/ack-lite/ack-lite.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 module Hipe
   module AckLite
     module Failey; end
@@ -7,39 +9,74 @@ module Hipe
     # kind of ridiculous to call it a 'Service' or a 'Proxy' but the
     # idea is that this is your only interface to it, this and Cli
 
-    class Request < Struct(:files, :folders, :regex, :regex_opts, :path); end
-    class ValidRequest < Struct(:files, :folders, :regex, :path); end
+    class Request < Struct.new(:files, :dirs, :path, :regexp_str, :regexp_opts_str); end
+    class ValidRequest < Struct.new(:files, :dirs, :path, :regexp); end
     module Service
-      def search request
-        valid = validate request
-      end
+      class << self
+        def search request
+          valid = validate request
+        end
 
-      def validate request
-        valid = ValidRequest.new
-        if ! File.dir?(request.path)
-          raise Fail.new("path must exist: #{request.path}")
+        # from Textmate:
+        # escape text to make it useable in a shell script as one “word” (string)
+        def e_sh(str)
+        	str.to_s.gsub(/(?=[^a-zA-Z0-9_.\/\-\x7F-\xFF\n])/n, '\\').gsub(/\n/, "'\n'").sub(/^$/, "''")
         end
-        valid.path = request.path
-        opts = 0
-        request.regex_opts.each do |letter|
-          case letter
-            when 'i': opts |= Regexp::IGNORECASE
-            when 'm': opts |= Regexp::MULTILINE
-            when 'x': opts |= Regexp::EXTENDED
-            else raise new Fail("unrecognized option: #{letter}." <<
-              "Expecting 'i','m', or 'x'")
+
+        def find_cmd request
+          cmd = "find -L #{e_sh request.path} "
+          and_me = []
+          if 0 < request.dirs.length
+            and_me << (' -not \( -type d \(' << (request.dirs.map{|x| "-name #{x}"} * ' -o ') <<
+              ' \) -prune  \)' )
           end
+          if 0 < request.files.length
+            and_me <<  ('\( ' << (request.files.map{|x| "-name \"#{x}\""} * ' -o ') << ' \)' )
+          end
+          cmd << ( and_me * ' -a ')
+          cmd
         end
-        begin
-          regex = Regex.new(request.regex, opts)
-        rescue RegexpError => e
-          e.extend Failey
-          raise e
+
+        def files request
+          cmd = find_cmd request
+          stdin, stdout, stderr = Open3.popen3(cmd)
+          out = stdout.read
+          err = stderr.read
+          err.strip!
+          if (0 < err.length)
+            raise Fail.new(err)
+          end
+          out.split("\n")
         end
-        valid.regex = regex
-        valid.folders = folders
-        valid.files = files
-        valid
+
+        def validate request
+          valid = ValidRequest.new
+          if ! File.directory?(request.path)
+            raise Fail.new("path must exist: #{request.path}")
+          end
+          valid.path = request.path
+          opts = 0
+          letters = request.regexp_opts_str.split('')
+          letters.each do |letter|
+            case letter
+              when 'i': opts |= Regexp::IGNORECASE
+              when 'm': opts |= Regexp::MULTILINE
+              when 'x': opts |= Regexp::EXTENDED
+              else raise Fail.new(%|unrecognized option: '#{letter}'. | <<
+                "Expecting 'i','m', or 'x'")
+            end
+          end
+          begin
+            regexp = Regexp.new(request.regexp_str, opts)
+          rescue RegexpError => e
+            e.extend Failey
+            raise e
+          end
+          valid.regexp = regexp
+          valid.dirs = request.dirs
+          valid.files = request.files
+          valid
+        end
       end
     end
   end
--- Support/external/ack-lite/test/all.rb
+++ Support/external/ack-lite/test/all.rb
@@ -1,13 +1,37 @@
 require 'minitest/autorun'  # unit and spec
-module Hipe
-  module AckLite
+require 'ruby-debug'
+_Root = File.expand_path(File.dirname(__FILE__)+'/..')
+require File.join(_Root,'/ack-lite')
+
+describe Hipe::AckLite do
+  it "should build request" do
+    request = Hipe::AckLite::Request.new([], [], '.','foo', '')
+    validated = Hipe::AckLite::Service.validate(request)
+    re = /foo/
+    re.must_equal validated.regexp
   end
-  describe AckLite do
-    it "should pass" do
-      1.must_equal 1
-    end
-    it "should fail" do
-      1.must_equal 2
-    end
+
+  it "should fail on bad request with opts" do
+    request = Hipe::AckLite::Request.new([], [], '.','foo', 'abc')
+    e = proc do
+      validated = Hipe::AckLite::Service.validate(request)
+    end.must_raise Hipe::AckLite::Failey
+    e.message.must_match %r{'a'\. Expecting 'i','m', or 'x'}
   end
+
+  it "should return list of files" do
+    path = './test/data'
+    # path = File.join(_Root, 'test')
+    # FileUtils.cd(path)
+    request = Hipe::AckLite::Request.new(['*.def'],[],path)
+    files = Hipe::AckLite::Service.files(request)
+    tgt = Set.new([
+      "./test/data/do-me/xyz.def", "./test/data/uvw.def", "./test/data/abc.def"
+    ])
+    have = files.to_set
+    (have.subset? tgt).must_equal true
+    (tgt.subset? have).must_equal true
+  end
+
+
 end
